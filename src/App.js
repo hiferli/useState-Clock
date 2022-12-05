0c78314683fa036b58940ecb7b4e4eded1353b12
IshaanJoshi <joshi.ishaan.2001@gmail.com>
2022-12-05 14:33:49 +0530

    Update Date

--- src/App.js
+++ src/App.js
@@ -3,19 +3,29 @@ import './App.css';
 import React , {useState} from 'react';
 
 function App() {
-  let time = new Date().toLocaleTimeString ();
+  let time = new Date().toLocaleTimeString();
+  let date = new Date().toLocaleDateString();
+
   const [currentTime, setcurrentTime] = useState(time);
-  
+  const [currentDate, setCurrentDate] = useState(date);
+
   const updateTIme = () => {
     time = new Date().toLocaleTimeString();
     setcurrentTime(time);
   }
+
+  const updateDate = () => {
+    date = new Date().toLocaleDateString();
+    setCurrentDate(date);
+  }
   
   setInterval(updateTIme , 1000);
+  setInterval(updateDate , 36000);
   
   return (
     <>
     <h1>{currentTime}</h1>
+    <h2>{currentDate}</h2>
     {/* <button onClick={updateTIme}>Get Current Time</button> */}
     </>
   );
